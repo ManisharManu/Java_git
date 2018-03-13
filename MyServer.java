3bdbf214355c89a4b4c77418c19ed7c9bf4a0b5c
ManisharManu <37325814+ManisharManu@users.noreply.github.com>
2018-03-13 14:31:12 +0530

    Create MyServer.java

---
+++ MyServer.java
@@ -0,0 +1,9 @@
+ import java.net.*;  
+    import java.io.*;  
+    class MyServer{  
+    public static void main(String args[])throws Exception{  
+    ServerSocket ss=new ServerSocket(3333);  
+    Socket s=ss.accept();  
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
